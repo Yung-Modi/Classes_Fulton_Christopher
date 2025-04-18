e164889bb9d9b31c1f86267e480950ee8b439219
Yung-Modi <chrisreaper404@gmail.com>
2025-04-18 13:08:37 -0500

    h

---
+++ Classes_Fulton_Christopher/Greetings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Classes_Fulton_Christopher
+{
+    class Greetings
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Welcome()
+        {
+            Console.WriteLine("Hello Traveler, welcome!");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        public void Hello(string name)
+        {
+            Console.WriteLine($"{name}, Glad you could join us today!)");
+        }
+
+    }
+
+}
--- Classes_Fulton_Christopher/Program.cs
+++ Classes_Fulton_Christopher/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Classes_Fulton_Christopher
+﻿using System;
+
+namespace Classes_Fulton_Christopher
 {
-    internal class Program
+    class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Greetings greetings = new Greetings();
+            greetings.Welcome();
+
+            Console.WriteLine("What is your name stranger?");
+            string userName = Console.ReadLine();
+
+            greetings.Hello(userName);
         }
     }
 }
